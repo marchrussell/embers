fca849f4b23f019d4d41790b1db86c5842281d3f
exchai93 <echai93@gmail.com>
2026-03-30 15:48:47 +0100

    chore: posthog config

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -7,6 +7,7 @@ export const initPostHog = () => {
   if (typeof window !== "undefined" && POSTHOG_KEY) {
     posthog.init(POSTHOG_KEY, {
       api_host: POSTHOG_HOST,
+      ui_host: "https://eu.posthog.com",
       person_profiles: "identified_only",
       capture_pageview: true,
       capture_pageleave: true,
